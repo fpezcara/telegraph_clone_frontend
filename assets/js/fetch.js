b063ec4ecb68ef687c3dee6d6be3d0527701af79
Florencia Pezcara <fpezcara@gmail.com>
2022-08-19 15:27:13 +0100

    update fetch paths

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,17 +1,19 @@
 const port = 3002;
 
 const fetchPost = async (title) => {
-    const response = await fetch(`http://localhost:${port}/posts/${title}`)
-    const post = await response.json();
-    return post;
-
-}
+  const response = await fetch("https://telegraph-api.herokuapp.com/posts");
+  const post = await response.json();
+  return post;
+};
 
 const fetchToCreatePost = async (title, name, story, url, picture) => {
-    const options = {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ title, name, story, url, picture })
-    }
-    const response = await fetch(`http://localhost:${port}/posts`, options);
-}
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, name, story, url, picture }),
+  };
+  const response = await fetch(
+    `https://telegraph-api.herokuapp.com/posts`,
+    options
+  );
+};
